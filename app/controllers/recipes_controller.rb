def289a65f54fa0695c06895c1212086ef7d2bdf
Liam Killion <liam.killion@gmail.com>
2017-11-12 14:41:29 -0500

    Done.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -9,8 +9,28 @@ class RecipesController < ApplicationController
 
   def new
     @recipe = Recipe.new
+    @recipe.ingredients.build
+    @recipe.ingredients.build
   end
 
+
   def create
+    recipe = Recipe.create(recipe_params)
+    redirect_to recipes_path
+  end
+
+  private
+
+ # {"utf8"=>"✓", "recipe"=>
+ #    {"title"=>"Chocolate Cake",
+ #      "ingredients_attributes"=>
+ #        {
+ #          "0"=>{"name"=>"", "quantity"=>""},
+ #          "1"=>{"name"=>"", "quantity"=>""}
+ #        }
+ #      },
+ #        "commit"=>"Create Recipe", "controller"=>"recipes", "action"=>"create"}
+  def recipe_params
+    params.require(:recipe).permit(:title, ingredients_attributes: [:quantity,:name])
   end
 end
